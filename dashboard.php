15cfad629e363391f0d8b40be324158186dfec32
NWJ9PB <inspiregaming1025@gmail.com>
2021-02-07 18:18:06 +0800

    Update dashboard.php

---
+++ dashboard.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php require("includes/assets.php");?>
+
+    <title>FirePanel</title>
+</head>
+
+<body class="bg-default shadow-none">
+    <?php require('includes/sidenav.php');?> 
+    
+</body>
+
+</html>
