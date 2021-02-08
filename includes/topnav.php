c447df1e909b35f079513b6976692fcfdebef5b5
NWJ9PB <inspiregaming1025@gmail.com>
2021-02-08 17:14:59 +0800

    Update topnav.php

--- includes/topnav.php
+++ includes/topnav.php
@@ -4,9 +4,8 @@
             <!-- Navbar links -->
             <ul class="navbar-nav align-items-center  ml-md-auto ">
                 <li class="nav-item d-xl-none">
-                    <!-- Sidenav toggler -->
-                    <div class="pr-3 sidenav-toggler sidenav-toggler-dark" data-action="sidenav-pin"
-                        data-target="#sidenav-main">
+                <!-- Sidenav toggler -->
+                    <div class="pr-3 sidenav-toggler sidenav-toggler-dark active" data-action="sidenav-pin" data-target="#sidenav-main">
                         <div class="sidenav-toggler-inner">
                             <i class="sidenav-toggler-line"></i>
                             <i class="sidenav-toggler-line"></i>
