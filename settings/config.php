b68a504759aeaf994cc1d288bce18a21f410f4f5
NWJ9PB <inspiregaming1025@gmail.com>
2021-02-08 17:15:25 +0800

    Upload `settings` folder

---
+++ settings/config.php
@@ -0,0 +1,16 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "";
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+//Pterodactyl
+$ptero_domain = "ass.com";
+$ptero_key = "dgssdasd";
+
+// Discord server
+$discord['autojoin_role'] = ""; //role ID
+$discord['autojoin_guildid'] = ""; //server ID
+$discord['bot_token'] = "";
+?>
