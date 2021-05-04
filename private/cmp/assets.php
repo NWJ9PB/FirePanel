8e98d17330b6cc79df3d8e9aa2aa1ced3afaa7d4
NWJ9PB <inspiregaming1025@gmail.com>
2021-05-04 12:35:28 +0800

    Changing design system to SoftUI Bootstrap 5

--- private/cmp/assets.php
+++
@@ -1,7 +0,0 @@
-<meta charset="UTF-8">
-<meta name="viewport" content="width=device-width, initial-scale=1.0">
-<meta name="description" content="FirePanel">
-<meta name="author" content="InterStellar#1704">
-<meta http-equiv="X-UA-Compatible" content="IE=7">
-<link rel="stylesheet" href="assets/vendor/fontawesome/css/all.css">
-<link rel="stylesheet" href="assets/css/argon.css?v=1.2.0" type="text/css">
