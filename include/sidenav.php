5f7eeadbb22c3f3cd2ba37963cf8705d280749e3
NWJ9PB <inspiregaming1025@gmail.com>
2021-05-06 16:40:26 +0800

    Create sidenav.php

---
+++ include/sidenav.php
@@ -0,0 +1,110 @@
+<aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-2 fixed-left ms-2 bg-dark rounded" id="sidenav-main">
+        <div class="sidenav-header">
+            <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute right-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
+            <a class="navbar-brand m-0" href="dashboard.html">
+                <img class="navbar-brand-img h-100 w-15" src="assets/img/logo-ct.png" >
+                <span class="ms-1 font-weight-bold text-white">Hosting</span>
+            </a>
+        </div>
+        <hr class="horizontal dark mt-0">
+        <div class="" id="sidenav-collapse-main">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active bg-gradient-faded-dark-vertical" href="index.php">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="fas fa-tachometer-alt fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">Dashboard</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.html">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="fas fa-server fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">My Servers</span>
+                    </a>
+                </li>
+                <li class="nav-item mt-3">
+                    <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-6">Account</h6>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.html">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="fas fa-user-plus fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">My Plan</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.html">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="fas fa-user-shield fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">My Account</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.html">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="far fa-life-ring fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">Get Support</span>
+                    </a>
+                </li>
+                <li class="nav-item mt-3">
+                    <h6 class="ps-4 ms-2 text-uppercase text-xs text-white font-weight-bolder opacity-6">Service Status</h6>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.html">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="fab fa-earlybirds fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">Pterodactyl Service Status</span>
+                    </a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="dashboard.html">
+                        <div class="shadow-none border-radius-md bg-if text-center me-2 d-flex align-items-center justify-content-center">
+                            <span class="fa-stack fa-sm">
+                                <i class="fas fa-square fa-stack-2x"></i>
+                                <i class="fab fa-earlybirds fa-stack-1x fa-inverse"></i>
+                            </span>
+                        </div>
+                        <span class="nav-link-text ms-1">Website Service Status</span>
+                    </a>
+                </li>
+            </ul>
+        </div>
+        <div class="sidenav-footer mx-3 mt-3 pt-3">
+            <div class="card card-background shadow-none card-background-mask-info" id="sidenavCard">
+                <div class="full-background" style="background-image: url(https://c0.wallpaperflare.com/preview/600/243/618/javascript-vscode-editor-code.jpg)"></div>
+                <div class="card-body text-left p-3 w-100">
+                    <div class="icon icon-shape icon-sm bg-white shadow text-center mb-3 d-flex align-items-center justify-content-center border-radius-md">
+                        <i class="ni ni-diamond text-dark text-gradient text-lg top-0" aria-hidden="true" id="sidenavCardIcon"></i>
+                    </div>
+                    <h6 class="text-white up mb-0">Want powerful servers?</h6>
+                    <p class="text-xs font-weight-bold">Check our services here</p>
+                    <a href="https://www.creative-tim.com/learning-lab/bootstrap/license/soft-ui-dashboard" target="_blank" class="btn btn-white btn-sm w-100 mb-0">Paid Hosting</a>
+                </div>
+            </div>
+        </div>
+    </aside>
