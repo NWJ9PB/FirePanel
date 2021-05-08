12ad9685d223868081bf76aabd816fe03793e7b3
NWJ9PB <inspiregaming1025@gmail.com>
2021-05-08 17:07:03 +0800

    Removed the page name on the topbar

--- include/topbar.php
+++ include/topbar.php
@@ -1,12 +1,8 @@
-<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-xl" id="navbarBlur" navbar-scroll="true">
-            <div class="container-fluid py-1 px-3">
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb bg-transparent mb-0 pb-0 pt-1 px-0 me-sm-6 me-5">
-                        <li class="breadcrumb-item text-sm"><a class="opacity-5 text-white" href="javascript:;">Pages</a></li>
-                        <li class="breadcrumb-item text-sm text-white active" aria-current="page">Billing</li>
-                    </ol>
-                    <h6 class="font-weight-bolder mb-0 text-white">Billing</h6>
-                </nav>
+<nav class="navbar navbar-main navbar-expand-lg px-0 mx-4 shadow-none border-radius-sm" id="navbarBlur" navbar-scroll="true">
+            <div class="container-fluid py-1 px-1">
+                <div>
+                    <span class="text-success text-bold border border-2 border-radius-lg border-success rounded p-2 my-2">SUCCESS: SERVER CREATED SUCCESSFULLY</span>
+                </div>
                 <div class="collapse navbar-collapse mt-sm-0 mt-2 me-md-0 me-sm-4" id="navbar">
                     <div class="ms-md-auto pe-md-3 d-flex align-items-center">
                         <div class="input-group">
