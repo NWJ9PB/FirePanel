514f7e90fe9e1c4d36f00fce5ba5e3bad7513f92
NWJ9PB / InterStellar#1704/3423 <helodibalado@gmail.com>
2021-05-28 08:58:47 +0800

    Added images to the info cards

---
+++ index.php
@@ -0,0 +1,273 @@
+<?php
+require('settings/initial.php');
+require('include/copyright.php');
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Overview</title>
+    <?php require('include/header.php');?>
+
+</head>
+
+<body class="g-sidenav-show bg-dark">
+    <?php require('include/sidenav.php');?>
+
+    <main class="main-content mt-1 border-radius-lg">
+        <?php require('include/topbar.php');?>
+
+        <div class="container-fluid py-4">
+            <div class="row">
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card card-background shadow-none card-background-mask-info border border-info" id="sidenavCard">
+                        <div class="full-background" style="background-image: url(https://images.unsplash.com/photo-1526304640581-d334cdbbf45e?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80)"></div>
+                        <div class="card-body text-left p-3 w-100">
+                            <div class="row">
+                                <div class="col-9">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Remaining Balance</p>
+                                        <h5 class="font-weight-bolder mb-0 text-white">$12.07</h5>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-end">
+                                    <div class="icon icon-shape icon-sm bg-gradient-danger shadow text-center border-radius-md">
+                                        <i class="fas fa-book mt-n12" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="badge badge-pill bg-warning"><i class="fas fa-money-bill ml-1 mr-2"></i> Cash In</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card card-background shadow-none card-background-mask-info border border-info" id="sidenavCard">
+                        <div class="full-background" style="background-image: url(https://images.unsplash.com/photo-1527219525722-f9767a7f2884?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=752&q=80)"></div>
+                        <div class="card-body text-left p-3 w-100">
+                            <div class="row">
+                                <div class="col-9">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Current Plan</p>
+                                        <h5 class="font-weight-bolder mb-0 text-white">Premium</h5>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-end">
+                                    <div class="icon icon-shape icon-sm bg-gradient-danger shadow text-center border-radius-md">
+                                        <i class="fas fa-book mt-n12" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="badge badge-pill bg-warning"><i class="fas fa-book ml-1 mr-2"></i> Check Plans</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card card-background shadow-none card-background-mask-info border border-info" id="sidenavCard">
+                        <div class="full-background" style="background-image: url(https://images.unsplash.com/photo-1484557052118-f32bd25b45b5?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80)"></div>
+                        <div class="card-body text-left p-3 w-100">
+                            <div class="row">
+                                <div class="col-9">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">Server Slot Left</p>
+                                        <h5 class="font-weight-bolder mb-0 text-white">2 Slots</h5>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-end">
+                                    <div class="icon icon-shape icon-sm bg-gradient-danger shadow text-center border-radius-md">
+                                        <i class="fas fa-money-bill mt-n12" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="badge badge-pill bg-gradient-success"><i class="fas fa-server ml-1 mr-2"></i> Server List</span>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-3 col-sm-6 mb-xl-0 mb-4">
+                    <div class="card card-background shadow-none card-background-mask-info border border-info" id="sidenavCard">
+                        <div class="full-background" style="background-image: url(https://images.unsplash.com/photo-1512075135822-67cdd9dd7314?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=375&q=80)"></div>
+                        <div class="card-body text-left p-3 w-100">
+                            <div class="row">
+                                <div class="col-9">
+                                    <div class="numbers">
+                                        <p class="text-sm mb-0 text-capitalize font-weight-bold">MiniCoins</p>
+                                        <h5 class="font-weight-bolder mb-0 text-white">324</h5>
+                                    </div>
+                                </div>
+                                <div class="col-3 text-end">
+                                    <div class="icon icon-shape icon-sm bg-gradient-danger shadow text-center border-radius-md">
+                                        <i class="fas fa-money-bill mt-n12" aria-hidden="true"></i>
+                                    </div>
+                                </div>
+                            </div>
+                            <span class="badge badge-pill bg-warning"><i class="fas fa-coins ml-1 mr-2"></i> Earn</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-xl-6 col-md-12 col-sm-12 mb-4">
+                    <div class="card bg-gradient-secondary">
+                        <div class="table-responsive">
+                            <table class="table align-items-center mb-0">
+                                <thead>
+                                    <tr>
+                                        <th class="text-uppercase text-white text-xxs text-center font-weight-bolder">Server Name</th>
+                                        <th class="text-uppercase text-white text-xxs text-center font-weight-bolder">Node</th>
+                                        <th class="text-uppercase text-white text-xxs text-center font-weight-bolder">CPU</th>
+                                        <th class="text-uppercase text-white text-xxs text-center font-weight-bolder">Memory</th>
+                                        <th class="text-uppercase text-white text-xxs text-center font-weight-bolder">Disk</th>
+                                        <th class="text-uppercase text-white text-xxs text-center font-weight-bolder">Port</th>
+                                        <th class="text-white"></th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            <h6 class="text-xs text-white font-weight-bold mb-0 text-center">SMP</h6>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">NA-1</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">70%</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">1000 MB</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">4000 MB</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">2</p>
+                                        </td>
+                                        <td class="align-middle">
+                                            <a href="" class="badge bg-secondary text-white font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                                            Edit
+                                            </a>
+                                        </td>
+                                    </tr>
+                                    <tr>
+                                        <td>
+                                            <h6 class="text-xs text-white font-weight-bold mb-0 text-center">Skyblock</h6>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">NA-1</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">70%</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">1000 MB</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">4000 MB</p>
+                                        </td>
+                                        <td>
+                                            <p class="text-xs text-white font-weight-bold mb-0 text-center">2</p>
+                                        </td>
+                                        <td class="align-middle">
+                                            <a href="" class="badge bg-secondary text-white font-weight-bold text-xs" data-toggle="tooltip" data-original-title="Edit user">
+                                            Edit
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                    </div>
+                </div>
+                <div class="col-xl-6 col-md-12 col-sm-12 mb-4">
+                    <div class="card bg-gradient-dark">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="d-flex flex-column h-100">
+                                        <p class="mb-1 pt-2 text-bold text-white">Announcements</p>
+                                        <h5 class="font-weight-bolder text-white">Soft UI Dashboard</h5>
+                                        <p class="mb-5 text-white">From colors, cards, typography to complex elements, you will have a great experience at using this!</p>
+                                        <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-auto" href="">
+                                            Read More at Discord Server
+                                            <i class="fab fa-discord fa-lg ms-1" aria-hidden="true"></i>
+                                            <i class="fas fa-arrow-right fa-lg ms-1" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="row mt-3">
+                <div class="col-xl-12">
+                    <div class="card bg-gray">
+                        <div class="card-body p-3">
+                            <div class="row">
+                                <div class="col-12">
+                                    <div class="d-flex flex-column h-100">
+                                        <p class="mb-2 text-bold text-white">Advertisements</p>
+                                        <img src="https://cdn.discordapp.com/attachments/839600786362204190/846343132584476672/Gif_leggera_sfondo.gif?size=512" alt="">
+                                        <a class="text-body text-sm font-weight-bold mb-0 icon-move-right mt-2" href="">
+                                            Read more here
+                                            <i class="fas fa-arrow-right text-sm ms-1" aria-hidden="true"></i>
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <footer class="footer pt-3">
+                <div class="container-fluid">
+                <div class="row align-items-center justify-content-lg-between">
+                    <div class="col-lg-6 mb-lg-0 mb-4">
+                    <div class="copyright text-center text-sm text-muted text-lg-left">
+                        © <script>
+                        document.write(new Date().getFullYear())
+                        </script>
+                        <a href="https://www.creative-tim.com" class="font-weight-bold" target="_blank">Creative Tim</a>
+                        | Edited by 
+                        <a href="https://github.com/NWJ9PB" class="font-weight-bold text-white" target="_blank"><span style="color: yellow;">NWJ9PB / InterStellar#3423</span> <span class="text-muted">for</span> <span style="color: orange;">NExxT Development</span></a>
+                    </div>
+                    </div>
+                    <div class="col-lg-6">
+                    <ul class="nav nav-footer justify-content-center justify-content-lg-end">
+                        <li class="nav-item">
+                        <a href="https://www.creative-tim.com" class="nav-link text-muted" target="_blank">Service Status</a>
+                        </li>
+                        <li class="nav-item">
+                        <a href="https://www.creative-tim.com/presentation" class="nav-link text-muted" target="_blank">About Us</a>
+                        </li>
+                        <li class="nav-item">
+                        <a href="http://blog.creative-tim.com" class="nav-link text-muted" target="_blank">Blog</a>
+                        </li>
+                        <li class="nav-item">
+                        <a href="https://www.creative-tim.com/license" class="nav-link pe-0 text-muted" target="_blank">License</a>
+                        </li>
+                    </ul>
+                    </div>
+                </div>
+                </div>
+            </footer>
+        </div>
+        <script src="assets/js/core/popper.min.js"></script>
+        <script src="assets/js/core/bootstrap.min.js"></script>
+        <script src="assets/js/plugins/smooth-scrollbar.min.js"></script>
+        <script src="assets/js/soft-ui-dashboard.min.js?v=1.0.1"></script>
+        <script>
+            var win = navigator.platform.indexOf('Win') > -1;
+            if (win && document.querySelector('#sidenav-scrollbar')) {
+            var options = {
+                damping: '0.5'
+            }
+            Scrollbar.init(document.querySelector('#sidenav-scrollbar'), options);
+            }
+        </script>
+        <script async defer src="https://buttons.github.io/buttons.js"></script>
+        <script src="assets/vendor/smooth-scrollbar-8.6.2/smooth-scrollbar.js"></script>
+
+
+        
+
