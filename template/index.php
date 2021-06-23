f4f96ba19d7cb55d88f1243400a28c8ace474e8b
NWJ9PB / InterStellar#1704/3423 <helodibalado@gmail.com>
2021-06-23 12:26:24 +0800

    SoftUI Template
    Action: Move

---
+++ template/index.php
@@ -0,0 +1,27 @@
+<?php
+require('settings/initial.php');
+require('include/copyright.php');
+$pageid = 1;
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <title>Overview</title>
+    <?php require('include/header.php');?>
+
+</head>
+
+<body class="g-sidenav-show bg-dark">
+    <?php require('include/sidenav.php');?>
+
+    <main class="main-content mt-1 border-radius-lg">
+        <?php require('include/topbar.php');?>
+
+        <div class="container-fluid py-4">
+            <div class="row mb-3">
+            </div>
+            <?php require('include/footer.php');?>
+        </div>
+        <?php require('include/scripts.php');?>
