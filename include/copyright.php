2e91fa4dbd862c1471100ebb276709de4e735e98
NWJ9PB / InterStellar#1704/3423 <helodibalado@gmail.com>
2021-06-27 04:30:38 +0800

    Update copyright.php

--- include/copyright.php
+++ include/copyright.php
@@ -1,22 +1,37 @@
 <!--
-    =========================================================
-    * Soft UI Dashboard  - v1.0.1
-    * FirePanel FrontEnd - <?php echo($commitdata->commit)?>
-
-    * FirePanel BackEnd  - v0.1.0
-    * FirePanel Package  - Basic-OpenSource
-    =========================================================
-
-    * Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
-    * Copyright 2021 Creative Tim (https://www.creative-tim.com)
-    * Licensed under MIT (https://github.com/creativetimofficial/soft-ui-dashboard/blob/master/LICENSE.md)
-
-    * Coded by Creative Tim
-    * Modified by NWJ9PB for FirePanel
-    * https://github.com/NExxT-Development/FirePanel
-    * https://github.com/NWJ9PB/FirePanel
-
-    =========================================================
-
-    * The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
+    // * ==============================================================================================================================
+    // * |
+    // * |                                               FIREPANEL WEB SOFTWARE
+    // * |
+    // * ==============================================================================================================================
+    // * ==============================================================================================================================
+    // * | Soft UI Dashboard Version  - 1.0.1
+    // * |     Bootstrap Version      - 5
+    // * |     FirePanel Version      - dev-unstable
+    // * |     FirePanel Package      - Unavailable
+    // * ==============================================================================================================================
+    // * |
+    // * | Product Page: https://www.creative-tim.com/product/soft-ui-dashboard
+    // * | Copyright 2021 Creative Tim (https://www.creative-tim.com)
+    // * | Licensed under MIT (https://github.com/creativetimofficial/soft-ui-dashboard/blob/master/LICENSE.md)
+    // * |
+    // * | Coded by Creative Tim
+    // * | Modified by NWJ9PB for FirePanel
+    // * | https://github.com/NExxT-Development/FirePanel     [ PUBLIC REPO | ALL PULL REQUEST/ISSUES MUST DONE IN THIS REPO]
+    // * | https://github.com/NWJ9PB/FirePanel                [  BASE REPO  |      ISSUES WILL BE AUTOMATICALLY CLOSED      ]
+    // * |
+    // * ==============================================================================================================================
+    // * | 
+    // * | Fonts Used: Open Sans Font
+    // * |
+    // * |
+    // * |
+    // * | Licensed under the Apache License Version 2.0
+    // * | Available at https://fonts.google.com
+    // * |
+    // * ==============================================================================================================================
+    // * |
+    // * |                                FULL LIST OF THIS COPYRIGHT IS AVAILABLE ON THE copyright FILE
+    // * |
+    // * ==============================================================================================================================
 -->
